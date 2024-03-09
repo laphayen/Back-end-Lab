942066f77a6f9b278186020863b9a1fca1d2a4fb
Baek Gi Chan <laphayen@gmail.com>
2024-03-09 22:37:18 +0900

    Test: Create QueryString with Key and Value

--- src/test/java/org/example/QueryStringTest.java
+++ src/test/java/org/example/QueryStringTest.java
@@ -6,9 +6,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class QueryStringTest {
 
+    // List<QueryString>
     @Test
     void createTest() {
-        QueryString queryString = new QueryString("operand1=7");
+        QueryString queryString = new QueryString("operand1", "7");
 
         assertThat(queryString).isNotNull();
     }
