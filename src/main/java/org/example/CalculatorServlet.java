03324d153feca704909567adca88d2578292dc29
Baek Gi Chan <laphayen@gmail.com>
2024-03-14 23:29:57 +0900

    Feat: Implement calculator service in Servlet

--- src/main/java/org/example/CalculatorServlet.java
+++ src/main/java/org/example/CalculatorServlet.java
@@ -1,11 +1,14 @@
 package org.example;
 
+import org.example.calculator.domain.Calculator;
+import org.example.calculator.domain.PositiveNumber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/calculate")
 public class CalculatorServlet implements Servlet {
@@ -18,8 +21,16 @@ public class CalculatorServlet implements Servlet {
     }
 
     @Override
-    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
         log.info("service");
+        int operand1 = Integer.parseInt(request.getParameter("operand1"));
+        String operator = request.getParameter("operator");
+        int operand2 = Integer.parseInt(request.getParameter("operand2"));
+
+        int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
+
+        PrintWriter writer = response.getWriter();
+        writer.println(result);
     }
 
     @Override
