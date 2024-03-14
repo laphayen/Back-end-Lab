eee5971b3ec590eb89a2cebd4bbbb2cbf472bc1b
Baek Gi Chan <laphayen@gmail.com>
2024-03-15 01:03:59 +0900

    Test: Add UserDaoTest

---
+++ src/test/java/org/example/UserDaoTest.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import java.util.ResourceBundle;
+
+public class UserDaoTest {
+
+    @BeforeEach
+    void setUp() {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("db_schema.sql"));
+        DatabasePopulatorUtils.execute(populator, ConnectionManager.getDataSource());
+    }
+}
