6018db1a6ff7db36707fad20f34a146be1ee347c
Baek Gi Chan <laphayen@gmail.com>
2024-03-15 15:44:55 +0900

    Test: Add createTest() to UserDaoTest

--- src/test/java/org/example/UserDaoTest.java
+++ src/test/java/org/example/UserDaoTest.java
@@ -1,12 +1,15 @@
 package org.example;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.DatabasePopulator;
 import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 import java.util.ResourceBundle;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserDaoTest {
 
@@ -15,5 +18,15 @@ public class UserDaoTest {
         ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
         populator.addScript(new ClassPathResource("db_schema.sql"));
         DatabasePopulatorUtils.execute(populator, ConnectionManager.getDataSource());
+    }
+
+    @Test
+    void createTest() {
+        UserDao userDao = new UserDao();
+
+        userDao.create(new User("laphayen", "password", "name", "email"));
+
+        User user = userDao.findByUserId("laphayen");
+        assertThat(user).isEqualTo(new User("laphayen", "password", "name", "email"));
     }
 }
