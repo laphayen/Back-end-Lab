40ec96d6fd9ad88955cdc10e56015e5af9752b6b
Baek Gi Chan <laphayen@gmail.com>
2024-03-15 23:15:17 +0900

    Refactor: Change Database Connection Pool

--- src/main/java/org/example/UserDao.java
+++ src/main/java/org/example/UserDao.java
@@ -4,24 +4,12 @@ import java.sql.*;
 
 public class UserDao {
 
-    public Connection getConnection() {
-        String url = "jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1";
-        String id = "sa";
-        String pw = "";
-
-        try {
-            Class.forName("org.h2.Driver");
-            return DriverManager.getConnection(url, id, pw);
-        } catch (Exception ex) {
-            return null;
-        }
-    }
 
     public void create(User user) throws SQLException {
         Connection con = null;
         PreparedStatement pstmt = null;
         try {
-            con = getConnection();
+            con = ConnectionManager.getConnection();
             String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
             pstmt = con.prepareStatement(sql);
             pstmt.setString(1, user.getUserId());
@@ -46,7 +34,7 @@ public class UserDao {
         PreparedStatement pstmt = null;
         ResultSet rs = null;
         try {
-            con = getConnection();
+            con = ConnectionManager.getConnection();
             String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
             pstmt = con.prepareStatement(sql);
             pstmt.setString(1, userId);
