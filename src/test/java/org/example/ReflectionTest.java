421dbe28b85276a98beec697a46d703ad126491e
Baek Gi Chan <laphayen@gmail.com>
2024-03-17 22:41:17 +0900

    Test: Retrieve an object of a loaded class type from the heap and verify if it's the same object

--- src/test/java/org/example/ReflectionTest.java
+++ src/test/java/org/example/ReflectionTest.java
@@ -9,12 +9,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.annotation.Annotation;
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.as;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ReflectionTest {
 
@@ -35,6 +37,27 @@ public class ReflectionTest {
         logger.debug("User all declared fields: [{}]", Arrays.stream(clazz.getDeclaredFields()).collect(Collectors.toList()));
         logger.debug("User all declared constructors: [{}]", Arrays.stream(clazz.getConstructors()).collect(Collectors.toList()));
         logger.debug("User all declared methods: [{}]", Arrays.stream(clazz.getDeclaredMethods()).collect(Collectors.toList()));
+    }
+
+    @Test
+    void load() throws ClassNotFoundException {
+        // 힙 영역에 로드되어 있는 클래스 타입의 객체 가져오기 1
+        Class<User> clazz1 = User.class;
+
+        // 힙 영역에 로드되어 있는 클래스 타입의 객체 가져오기 2
+        User user = new User("laphayen", "qwe");
+        Class<? extends User> clazz2 = user.getClass();
+
+        // 힙 영역에 로드되어 있는 클래스 타입의 객체 가져오기 3
+        Class<?> clazz3 = Class.forName("org.example.model.User");
+
+        logger.debug("clazz: [{}]", clazz1);
+        logger.debug("clazz: [{}]", clazz2);
+        logger.debug("clazz: [{}]", clazz3);
+
+        assertThat(clazz1 == clazz2).isTrue();
+        assertThat(clazz2 == clazz3).isTrue();
+        assertThat(clazz3 == clazz1).isTrue();
     }
 
     private static Set<Class<?>> getTypesAnnotatedWith(List<Class<? extends Annotation>> anootations) {
