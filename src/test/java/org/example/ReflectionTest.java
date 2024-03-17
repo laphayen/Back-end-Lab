70b04fc36f858293dc304a57f6124753d3ac0dcc
Baek Gi Chan <laphayen@gmail.com>
2024-03-17 13:15:29 +0900

    Test: Apply Logger to ReflectionTest

--- src/test/java/org/example/ReflectionTest.java
+++ src/test/java/org/example/ReflectionTest.java
@@ -3,11 +3,16 @@ package org.example;
 import org.example.annotation.Controller;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashSet;
 import java.util.Set;
 
 public class ReflectionTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+
     @Test
     void controllerScan() {
         Reflections reflections = new Reflections("org.example");
