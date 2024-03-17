53f7f21f66f51945eaebbfdce1c880e998248c51
Baek Gi Chan <laphayen@gmail.com>
2024-03-17 22:31:55 +0900

    Test: Add the showClass method

--- src/test/java/org/example/ReflectionTest.java
+++ src/test/java/org/example/ReflectionTest.java
@@ -2,15 +2,19 @@ package org.example;
 
 import org.example.annotation.Controller;
 import org.example.annotation.Service;
+import org.example.model.User;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ReflectionTest {
 
@@ -21,6 +25,16 @@ public class ReflectionTest {
         Set<Class<?>> beans = getTypesAnnotatedWith(List.of(Controller.class, Service.class));
 
         logger.debug("beans: [{}]", beans);
+    }
+
+    @Test
+    void showClass() {
+        Class<User> clazz = User.class;
+        logger.debug(clazz.getName());
+
+        logger.debug("User all declared fields: [{}]", Arrays.stream(clazz.getDeclaredFields()).collect(Collectors.toList()));
+        logger.debug("User all declared constructors: [{}]", Arrays.stream(clazz.getConstructors()).collect(Collectors.toList()));
+        logger.debug("User all declared methods: [{}]", Arrays.stream(clazz.getDeclaredMethods()).collect(Collectors.toList()));
     }
 
     private static Set<Class<?>> getTypesAnnotatedWith(List<Class<? extends Annotation>> anootations) {
